bd7dab76086aef95f5c70607855d33e4b448e111
ri-ree <adrie270801@gmail.com>
2023-12-16 16:05:29 +0100

    Add a loop to create each of the 16 divs

--- javascript.js
+++ javascript.js
@@ -1,1 +1,7 @@
 const container = document.querySelector('#maindiv');
+
+for (let i = 0; i <= 16; i++) {
+    const divs = document.createElement('div');
+    divs.className = "divs";
+    container.appendChild(divs);
+}
