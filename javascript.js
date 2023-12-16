c03cde78909d7df06d56aed6a7caa01700311b12
ri-ree <adrie270801@gmail.com>
2023-12-16 21:31:03 +0100

    Add a prompt event listener to the button

--- javascript.js
+++ javascript.js
@@ -2,6 +2,7 @@ const container = document.querySelector('#maindiv');
 
 const btn = document.querySelector('button');
 btn.addEventListener('click', () => {
+    prompt("Choose a number of square per side. (PS: 100 is the limit)");
 });
 
 for (let i = 0; i < 16; i++) {
