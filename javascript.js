57a4c35a4ca971554e57c3330053e283162e99cf
ri-ree <adrie270801@gmail.com>
2023-12-16 19:58:27 +0100

    Add divs style inside loop

--- javascript.js
+++ javascript.js
@@ -1,7 +1,9 @@
 const container = document.querySelector('#maindiv');
 
-for (let i = 0; i <= 16; i++) {
+for (let i = 0; i < 16; i++) {
     const divs = document.createElement('div');
     divs.className = "divs";
+    divs.style.border = '2px solid pink';
+    divs.style.padding = '50px';
     container.appendChild(divs);
 }
