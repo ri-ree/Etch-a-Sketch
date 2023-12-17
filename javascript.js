c8ca4eeb20aae50bbad290b5a824d6e341da257b
ri-ree <adrie270801@gmail.com>
2023-12-17 01:35:11 +0100

    Add function to clear container div
    The button was not clearing the div after it was clicked, so a function was added for that purpose

--- javascript.js
+++ javascript.js
@@ -1,11 +1,13 @@
 const container = document.querySelector('#maindiv');
+const btn = document.querySelector('#button');
 
-const btn = document.querySelector('button');
-btn.addEventListener('click', () => {
-    prompt("Choose a number of square per side. (PS: 100 is the limit)");
-});
+function clearBox(elementID)
+{
+    document.getElementById(elementID).innerHTML = "";
+}
 
-for (let i = 0; i < 16; i++) {
+
+    for (let i = 0; i < 16; i++) {
     const divs = document.createElement('div');
     divs.className = "divs";
     divs.style.border = '2px solid black';
@@ -15,4 +17,21 @@ for (let i = 0; i < 16; i++) {
         divs.style.background = 'red';
     });
     container.appendChild(divs);
-};
+    };
+
+
+
+    btn.addEventListener('click', () => {
+        clearBox("maindiv");
+        const input = Number(prompt("Choose a number of square per side. (PS: The limit is 100)"));
+        for (j = 0; j < input; j++) {
+        const newdivs = document.createElement('div');
+        newdivs.className = "newdivs";
+        newdivs.style.border = '2px solid black';
+        newdivs.style.flexGrow = 1;
+        newdivs.addEventListener("mouseover", () => {
+            newdivs.style.background = 'red';
+        });
+        container.appendChild(newdivs);
+        };
+    });
