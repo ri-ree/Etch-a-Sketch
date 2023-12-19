8c7aed38b99e456d6f31b4f1ea8ac9f803a46445
ri-ree <adrie270801@gmail.com>
2023-12-19 20:30:25 +0100

    Add a click event listener to the button
    The button needed to make a new grid appear, so an event listener was added with loops similar to newGrid and the input as a parameter

--- javascript.js
+++ javascript.js
@@ -2,6 +2,10 @@ const container = document.querySelector('#maindiv');
 const btn = document.querySelector('#button');
 
 
+function emptyDiv(divId) {
+    document.getElementById(divId).innerHTML = "";
+};
+
 function newGrid(size) {
     for (let i = 0; i < size; i++) {
     let allRows = document.createElement('div');
@@ -26,3 +30,28 @@ function newGrid(size) {
 };
 
 newGrid(16);
+
+btn.addEventListener('click', () => {
+    emptyDiv("maindiv");
+    const input = Number(prompt("Choose a number of square per size. (PS: The limit is 100)"));
+    for (let k = 0; k < input && k <= 99; k++) {
+        let theRows = document.createElement('div');
+        if (k % input == 0) {
+        theRows.style.marginTop = k * (500 / input) + "px";
+            };
+        for (let l = 0; l < input && l <= 99; l++) {
+            let theMultipleRows = document.createElement('div');
+            theMultipleRows.classList.add("theRows");
+            theMultipleRows.style.border = "1px solid black";
+            theMultipleRows.style.height = 500 / input + "px";
+            theMultipleRows.style.width = 500 / input + "px";
+            theMultipleRows.style.boxSizing = "border-box";
+            theMultipleRows.style.flex = 1;
+            theMultipleRows.addEventListener("mouseover", () => {
+                theMultipleRows.style.background = 'red';
+                });
+            theRows.appendChild(theMultipleRows);
+            };
+        container.appendChild(theRows);
+        };
+});
