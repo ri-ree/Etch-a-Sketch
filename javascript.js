0a466694e8dd3e0213649f0703e96cb54a0d4842
ri-ree <adrie270801@gmail.com>
2023-12-19 20:11:45 +0100

    Add an event listener to the divs
    The divs needed to change colors when the mouse was over them so an event listener was added.

--- javascript.js
+++ javascript.js
@@ -9,14 +9,17 @@ function newGrid(size) {
     allRows.style.marginTop = i * (500 / size) + "px";
         };
     for (let j = 0; j < size; j++) {
-        let multiplerows = document.createElement('div');
-        multiplerows.classList.add("rows");
-        multiplerows.style.border = "1px solid black";
-        multiplerows.style.height = 500 / size + "px";
-        multiplerows.style.width = 500 / size + "px";
-        multiplerows.style.boxSizing = "border-box";
-        multiplerows.style.flex = 1;
-        allRows.appendChild(multiplerows);
+        let multipleRows = document.createElement('div');
+        multipleRows.classList.add("rows");
+        multipleRows.style.border = "1px solid black";
+        multipleRows.style.height = 500 / size + "px";
+        multipleRows.style.width = 500 / size + "px";
+        multipleRows.style.boxSizing = "border-box";
+        multipleRows.style.flex = 1;
+        multipleRows.addEventListener("mouseover", () => {
+            multipleRows.style.background = 'red';
+            });
+        allRows.appendChild(multipleRows);
         };
     container.appendChild(allRows);
     };
